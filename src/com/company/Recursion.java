f619ba38aeddf9677a9985ab096a7c9785aa3c33
Sushant Mishra <mishrasushant1901@gmail.com>
2022-02-20 12:54:26 +0530

    print increasing

---
+++ src/com/company/Recursion.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.util.*;
+public class Recursion {
+
+    private static void printIncreasing(int n) {
+        if(n==0){
+            return;
+        }
+        printIncreasing(n-1);
+        System.out.println(n);
+
+    }
+    public static void main(String[] args) {
+         Scanner sc= new Scanner(System.in);
+         int n= sc.nextInt();
+         printIncreasing(n);
+
+    }
+
+}
